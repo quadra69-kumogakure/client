7e77ac0feb6e70d18ee42e5cad4ccb3e5a7ecb58
andaridea <deapuspita1796@gmail.com>
2024-04-24 06:52:35 +0700

    update layout for contactpage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,21 +1,25 @@
 
 function ContactPage() {
 
-    return (<>
+  return (<>
 
-        <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-            <div className="bg-white rounded-lg shadow-md p-6 max-w-md w-full">
-                <h1 className="text-2xl font-bold mb-6">Contact List</h1>
-                <ul>
-                        <li className="mb-4">
-                            <p className="text-lg font-semibold">Jhon Doe</p>
-                            <p className="text-gray-500">jhon.doe@mail.com</p>
-                        </li>
-                </ul>
-            </div>
-        </div>
+<div className="col-span-6 flex flex-col bg-slate-50 px-3 py-5 overflow-auto">
+  <h1 className="text-3xl font-extrabold text-gray-900 flex justify-center mb-4">Contacts</h1>
+  <ul role="list" className="p-6 divide-y divide-slate-200">
+    <li className="flex py-4 first:pt-0 last:pb-0 relative group">
+      <img className="h-10 w-10 rounded-full" src="http://dummyimage.com/147x100.png/dddddd/000000" alt="" />
+      <div className="ml-3 overflow-hidden">
+        <p className="text-sm font-medium text-slate-900">Jhon Doe</p>
+        <p className="text-sm text-slate-500 truncate">Jhon_doe@mail.com</p>
+      </div>
+    </li>
+  </ul>
+</div>
 
-    </>)
+
+
+
+  </>)
 };
 
 export default ContactPage;
