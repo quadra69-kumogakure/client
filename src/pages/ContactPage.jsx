03553d6af750aa2e240937c4181d48e9481ceded
andaridea <deapuspita1796@gmail.com>
2024-04-23 22:05:29 +0700

    add layout for contactpage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,20 @@
 
 function ContactPage() {
-    
+
     return (<>
+
+        <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+            <div className="bg-white rounded-lg shadow-md p-6 max-w-md w-full">
+                <h1 className="text-2xl font-bold mb-6">Contact List</h1>
+                <ul>
+                        <li className="mb-4">
+                            <p className="text-lg font-semibold">Jhon Doe</p>
+                            <p className="text-gray-500">jhon.doe@mail.com</p>
+                        </li>
+                </ul>
+            </div>
+        </div>
+
     </>)
 };
 
