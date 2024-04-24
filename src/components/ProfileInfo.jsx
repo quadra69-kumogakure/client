e55d826e434690ade497e9e05a4cfde1edbd066e
raihanonad <raihansetiawan3296@gmail.com>
2024-04-24 18:38:30 +0700

    return (     <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>       <MDBContainer className="py-5 h-100">         <MDBRow className="justify-content-center align-items-center h-100">           <MDBCol lg="6" className="mb-4 mb-lg-0">             <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>               <MDBRow className="g-0">                 <MDBCol md="4" className="gradient-custom text-center text-white"                   style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>                   <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"                     alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />                   <MDBTypography tag="h5">Marie Horwitz</MDBTypography>                   <MDBCardText>Web Designer</MDBCardText>                   <MDBIcon far icon="edit mb-5" />                 </MDBCol>                 <MDBCol md="8">                   <MDBCardBody className="p-4">                     <MDBTypography tag="h6">Information</MDBTypography>                     <hr className="mt-0 mb-4" />                     <MDBRow className="pt-1">                       <MDBCol size="6" className="mb-3">                         <MDBTypography tag="h6">Email</MDBTypography>                         <MDBCardText className="text-muted">info@example.com</MDBCardText>                       </MDBCol>                       <MDBCol size="6" className="mb-3">                         <MDBTypography tag="h6">Phone</MDBTypography>                         <MDBCardText className="text-muted">123 456 789</MDBCardText>                       </MDBCol>                     </MDBRow>
                        <MDBTypography tag="h6">Information</MDBTypography>
                        <hr className="mt-0 mb-4" />
                        <MDBRow className="pt-1">
                          <MDBCol size="6" className="mb-3">
                            <MDBTypography tag="h6">Email</MDBTypography>
                            <MDBCardText className="text-muted">info@example.com</MDBCardText>
                          </MDBCol>
                          <MDBCol size="6" className="mb-3">
                            <MDBTypography tag="h6">Phone</MDBTypography>
                            <MDBCardText className="text-muted">123 456 789</MDBCardText>
                          </MDBCol>
                        </MDBRow>
    
                        <div className="d-flex justify-content-start">
                          <a href="#!"><MDBIcon fab icon="facebook me-3" size="lg" /></a>
                          <a href="#!"><MDBIcon fab icon="twitter me-3" size="lg" /></a>
                          <a href="#!"><MDBIcon fab icon="instagram me-3" size="lg" /></a>
                        </div>
                      </MDBCardBody>
                    </MDBCol>
                  </MDBRow>
                </MDBCard>
              </MDBCol>
            </MDBRow>
          </MDBContainer>
        </section>
      );

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -6,10 +6,10 @@ function ProfileInfo({ userData }) {
     return (
         <div className="flex flex-col items-center gap-2 mb-5">
             <NavLink to="/update-profile">
-                <img
-                    src={userData.user.profilePicture} alt="..."
-                    className="shadow-sm hover:cursor-pointer rounded-full w-20 h-20 align-middle border-none object-cover"
-                />
+            <img 
+                src={userData.user.profilePicture} alt="..." 
+                className="shadow-sm rounded-full w-20 h-20 align-middle border-none object-cover" 
+            />
             </NavLink>
             <p className="font-semibold text-xl">{userData.user.firstName} {userData.user.lastName}</p>
         </div>
--- src/pages/MarketplacePage.jsx
+++ src/pages/MarketplacePage.jsx
@@ -1,5 +1,80 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { apiRequest } from "../utils/axios";
 
 function MarketplacePage() {
+    const navigate = useNavigate();
+    const [data, setData] = useState({});
+    const { id } = useParams();
+    //   console.log(id,'<<<<<');
+  
+    async function fetchData() {
+      try {
+        const { data } = await apiRequest({
+          url: `/stickers`,
+        });
+        // console.log(data,'<<<<<');
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    const [input, setInput] = useState({
+      title: "",
+      stickerId: 0,
+    });
+  
+    const handleInput = (event) => {
+      const { name, value } = event.target;
+      setInput({
+        ...input,
+        [name]: value,
+      });
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        input.stickerId=id
+        const { data } = await apiRequest({
+          method: "post",
+          url: `/stickers/${id}`,
+          headers: {
+            Authorization: "Bearer " + localStorage.access_token,
+          },
+          data: input,
+        });
+        // console.log(data.transactionToken,'<<<<<<<<<<<<<');
+        window.snap.pay(data.transactionToken, {
+          onSuccess: async function(result){
+            /* You may add your own implementation here */
+            console.log(result);
+            await apiRequest({
+              method: "post",
+              url: '/payment',
+              data: {
+                bookingId: data.bookingId
+              },
+              headers: {
+                Authorization: "Bearer " + localStorage.accessToken,
+              },
+            })
+          }
+        });
+  
+        // navigate("/booking/list");
+      } catch (error) {
+        console.log(error);
+        Swal.fire({
+          title: error.response.data.message,
+          icon: "error",
+        });
+      }
+    };
 
     return (
         <>
