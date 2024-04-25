f6b246950184a1023b5cccdb97631245f218040f
daffabaihaqi <baihaqidaff@gmail.com>
2024-04-25 13:49:47 +0700

    feat : client deployment

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const apiRequest = axios.create({
-    baseURL : "http://localhost:3000/"
-})
+    baseURL : "https://server-gp.daffabaihaqi.online"
+});
+
 
 export const serverRequest = axios.create({
     baseURL : ""
